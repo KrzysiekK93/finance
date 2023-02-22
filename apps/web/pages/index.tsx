97832dca51307803317e3da834db52e8005409e2
Krzysztof Kowalik <kowalik.k93@gmail.com>
2023-02-22 21:12:18 +0100

    Tailwind and daisyUI added

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,8 +3,8 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1 className="mb-2">Web</h1>
+      <button className="btn">Button</button>
     </div>
   );
 }
